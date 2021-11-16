df9511c6c3e79387ff3b2634f9d5174b1c4d8e10
Dmitry Nemov <katastrophe.der.leere@gmail.com>
2021-11-16 23:27:42 +0300

    HW_1 - 📫 HW_1

---
+++ HW_1_CONTENT/HW_1-2.7.py
@@ -0,0 +1,7 @@
+a = 2  # number of hours
+b = 3
+seconds = 3600  # number of seconds in 1 hour
+print('Hours: ', a)
+print('Second in Hours: ', a * seconds)
+print('Goodbye')
+print('Second in 3 hour: ', b * seconds)
---
+++ HW_1_CONTENT/HW_2.14.py
@@ -0,0 +1,6 @@
+HR_PER_DAY = 24
+SECOND_IN_HR = 3600
+
+days = 30
+
+print(days * HR_PER_DAY * SECOND_IN_HR)
